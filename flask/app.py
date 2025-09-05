d2de0e38489ce6519d16948463ec509e2860156e
Souvik Ghosh <souvik_ghosh@pursuitsoftware.com>
2025-09-05 03:16:50 -0500

    date2

--- flask/app.py
+++ flask/app.py
@@ -5,7 +5,7 @@ import threading
 import schedule
 import time
 from utils import check_and_send_reminders
-
+import os
 app = Flask(__name__)
 app.config.from_object(Config)
 
@@ -26,4 +26,5 @@ scheduler_thread.daemon = True
 scheduler_thread.start()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Use Render's assigned port
+    app.run(host='0.0.0.0', port=port)
