122d0f750262d21c48aecf6e125a71ba687130cf
Souvik Ghosh <souvik_ghosh@pursuitsoftware.com>
2025-09-08 13:40:03 +0530

    date2

--- flask/config.py
+++ flask/config.py
@@ -1,13 +1,14 @@
 import os
 from dotenv import load_dotenv
-
+TWILIO_ACCOUNT_SID='SK538b9e73d7c6c267addf51948adf4fcd'
+TWILIO_AUTH_TOKEN='BofCwRHaiEIFB9Gi9JfBpuLqapI2Qvqd'
 load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'dev-key-please-change')
     SUPABASE_URL = os.getenv('SUPABASE_URL')
     SUPABASE_KEY = os.getenv('SUPABASE_KEY')
-    TWILIO_ACCOUNT_SID = os.getenv('TWILIO_ACCOUNT_SID')
-    TWILIO_AUTH_TOKEN = os.getenv('TWILIO_AUTH_TOKEN')
+    TWILIO_ACCOUNT_SID = TWILIO_ACCOUNT_SID
+    TWILIO_AUTH_TOKEN = TWILIO_AUTH_TOKEN
     TWILIO_PHONE_NUMBER = os.getenv('TWILIO_PHONE_NUMBER')
     TWILIO_WHATSAPP_NUMBER = os.getenv('TWILIO_WHATSAPP_NUMBER')
